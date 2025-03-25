ee6c727ea10b94f4ddcde4944a107ec303a9c7f4
Peter Gangmei <peter.liangtuang@gmail.com>
2025-03-25 13:21:56 +0530

    Add Code of Conduct and fix ASGI configuration

--- core/asgi.py
+++ core/asgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+# Use production settings for ASGI deployments
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings.prod')
 
 application = get_asgi_application()
